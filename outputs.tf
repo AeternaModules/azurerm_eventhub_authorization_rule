b01d7a9a90b80e4923a5a704a393647241ec748d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "eventhub_authorization_rules_id" {
+  description = "Map of id values across all eventhub_authorization_rules, keyed the same as var.eventhub_authorization_rules"
+  value       = { for k, v in azurerm_eventhub_authorization_rule.eventhub_authorization_rules : k => v.id }
+}
 output "eventhub_authorization_rules_eventhub_name" {
   description = "Map of eventhub_name values across all eventhub_authorization_rules, keyed the same as var.eventhub_authorization_rules"
   value       = { for k, v in azurerm_eventhub_authorization_rule.eventhub_authorization_rules : k => v.eventhub_name }
